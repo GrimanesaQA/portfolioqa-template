348a9631832e0f21eb652899381bab6f45d4edcf
Grimanesa <grimanesa.lozano83@gmail.com>
2026-03-26 16:37:03 +0000

    Update update.sql

--- sql/update.sql
+++ sql/update.sql
@@ -1,19 +1,17 @@
--- Queries for updating and deleting data in form table
-
--- 1. Cambiar el email de laura
+-- Cambiar el email de laura por lauratest@gmail.com
 UPDATE form
 SET email = 'lauratest@gmail.com'
 WHERE name = 'laura';
 
--- 2. Cambiar edad 20 por 21
+-- Cambiar edad 20 por 21
 UPDATE form
 SET age = 21
 WHERE age = 20;
 
--- 3. Eliminar todos los registros con edad 30
+-- Eliminar todos los registros con edad 30
 DELETE FROM form
 WHERE age = 30;
 
--- 4. Eliminar todos los registros con nombre 'pedro'
+-- Eliminar todos los registros con nombre pedro
 DELETE FROM form
 WHERE name = 'pedro';
