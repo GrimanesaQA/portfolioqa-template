0d168d56c94569726a279fc6d26748627b0f7832
Grimanesa <grimanesa.lozano83@gmail.com>
2026-04-01 19:51:23 +0100

    Update update.sql

--- sql/update.sql
+++ sql/update.sql
@@ -7,11 +7,3 @@ WHERE name = 'laura';
 UPDATE form
 SET age = 21
 WHERE age = 20;
-
--- Eliminar todos los registros con edad 30
-DELETE FROM form
-WHERE age = 30;
-
--- Eliminar todos los registros con nombre pedro
-DELETE FROM form
-WHERE name = 'pedro';
